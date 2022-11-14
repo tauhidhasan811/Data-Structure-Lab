5abb6741544eb7b0c30c68ea9897acc37792142f
tauhidhasan811 <114634431+tauhidhasan811@users.noreply.github.com>
2022-11-14 11:58:16 +0600

    Add files via upload

---
+++ Final Lab-1 (Problem-1).cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+
+int queue[10], front = -1, back = -1, size=10;
+
+
+class Queue
+{
+public :
+    bool isEmpty()
+    {
+        if ((front == -1) && (back == -1))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool isFull()
+    {
+        if ((back +1)%size == front)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+public :
+    bool enqueue(int Element)
+    {
+        if(isFull())
+        {
+            cout<< "Queue is Full\n";
+            return false;
+        }
+        else if(isEmpty())
+        {
+            front = back = 0;
+            queue[back] = Element;
+            cout<< Element <<" is inserted into the Queue successfully.\n";
+            return true;
+        }
+        else
+        {
+            back++;
+            queue[back] = Element;
+            cout<< Element <<" is inserted into the Queue successfully.\n";
+            return true;
+        }
+    }
+public :
+    bool dequeue()
+    {
+        if(isEmpty())
+        {
+            cout << "\nSorry!!Queue is empty.\n";
+            return false;
+        }
+        else if (front == back)
+        {
+            front = back = -1;
+            cout << "Dequeue function is performed successfully.\n";
+            return true;
+        }
+         else
+         {
+              front=(front+1)% size;
+              cout << "Dequeued  successfully.\n";
+              return true;
+         }
+    }
+public :
+
+    int frontElement()
+    {
+        return queue[front];
+    }
+public :
+
+    int rearElement()
+    {
+         return queue[back];
+    }
+    void showQueue()
+    {
+
+        if (isEmpty() )
+        {
+             cout << "\nThe  Queue is empty\n";
+             return;
+        }
+        else
+        {
+            cout << "\nQUEUE ELEMENTS: ";
+            for(int i = front; i <= back; i++)
+            {
+               cout << queue[i] << " ";
+            }
+            cout << endl << endl;
+        }
+    }
+
+};
+
+int main()
+{
+    Queue q;
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+    q.enqueue(6);
+    q.enqueue(7);
+    q.enqueue(8);
+    q.enqueue(9);
+    q.showQueue();
+    cout<<"\nFront Element of the Queue: " << q.frontElement() <<endl;
+    cout<<"Rear Element of the Queue: " << q.rearElement() <<endl;
+
+    q.dequeue();
+    q.dequeue();
+    q.dequeue();
+
+    q.showQueue();
+    cout<<"\nFront Element of the Queue: " << q.frontElement() <<endl;
+    cout<<"Rear Element of the Queue: " << q.rearElement() <<endl;
+
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    q.showQueue();
+    cout<<"\nFront Element of the Queue: " << q.frontElement() <<endl;
+    cout<<"Rear Element of the Queue: " << q.rearElement() <<endl;
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    q.showQueue();
+    cout<<"\nFront Element of the Queue: " << q.frontElement() <<endl;
+    cout<<"Rear Element of the Queue: " << q.rearElement() <<endl;
+
+    //return 0;
+}
+
